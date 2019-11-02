1a78218ee3bd3938bdc5ff2326415c087e105c6c
esameth <esameth1@gmail.com>
2019-11-02 17:54:33 -0400

    Working - merges the child fasta files into the parent directory This parses through the json file and stores the file paths in lists

--- DFS.py
+++ DFS.py
@@ -1,37 +1,48 @@
 import os
+import json
 
-'''def walk(path):
-    for root, dirs, files in os.walk(path, topdown=False):
-        for file in files:
-            path = os.path.join(root.rsplit('/', 2) [0], "sequences", "multifasta.txt")
-            if file == "multifasta.txt":
-                fasta = os.path.join(root,file)
-            print(fasta, "----", path)
-
-            #create(fasta, path)
+# Open the json file
+def openFile():
+    with open("proteins.json") as f:
+        loaded_json = json.load(f)
+    return loaded_json
 
 
-def merge(arr):
-    for path in arr:
-        with open(path, 'r') as infile:
+# Merges the fasta files of the subdirectory into the parent directory
+# paths: list of file paths to be merged
+# parent: the parent path that the files will be merged into
+
+def merge(paths, parent):
+    for path in paths:
+        with open(path, 'r') as infile, open(parent, 'a+') as outfile:
             for line in infile:
                 outfile.write(line)
 
-    with open(fasta, 'r') as infile, open(path, "a+") as outfile:
-        for line in infile:
-            outfile.write(line)
-    "return path to parent directory"'''
-
-
-def dfs(path):
-    for f in os.scandir(path):
-        if f.is_dir():
-            dfs(f)
-            print(f.path)
+# Go through the dictionary and create lists of file paths to be merged
+# __path is the list of files to be merged
+# __pathP is the parent path to merge the file into
+def parseDict(proteins):
+    for x, x_dict in proteins.items():
+        Hpath = []
+        for h, h_dict in x_dict.items():
+            Tpath = []
+            Hpath.append(os.path.join(x, h,"sequences", "multifasta.txt"))
+            HpathP = os.path.join(x, "sequences", "multifasta.txt")
+            for t, t_dict in h_dict.items():
+                Fpath = []
+                Tpath.append(os.path.join(x, h, t, "sequences", "multifasta.txt"))
+                TpathP = os.path.join(x, h, "sequences", "multifasta.txt")
+                for f, f_dict in t_dict.items():
+                    Fpath.append(os.path.join(x, h, t, f, "sequences", "multifasta.txt"))
+                    FpathP = os.path.join(x, h, t, "sequences", "multifasta.txt")
+                merge(Fpath, FpathP)
+            merge(Tpath, TpathP)
+        merge(Hpath, HpathP)
 
 
 def main():
-    dfs("1")
+    proteins = openFile()
+    parseDict(proteins)
 
 
 if __name__ == "__main__":
