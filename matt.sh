bdfc1cdbc22d08b57ce6220669047fc8e3b5eb7a
esameth <46979903+esameth@users.noreply.github.com>
2019-11-14 08:02:02 -0500

    Bash script to run all of the programs

---
+++ matt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 parser.py
+echo Finished parser.py
+python3 directories.py
+echo Finished directories.py
+python3 DFS.py
+echo Finished DFS.py
+python3 pdb.py
+echo Finished pdb.py
