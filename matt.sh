67f937a4508622eca15df763575480d2943a8436
esameth <46979903+esameth@users.noreply.github.com>
2019-11-18 23:04:55 -0500

    Includes fasta.py

--- matt.sh
+++ matt.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 python3 parser.py
-echo Finished parser.py
+echo Finished creating dictionary
+python3 fasta.py
+echo Finished removing duplicates
 python3 directories.py
-echo Finished directories.py
+echo Finished creating directories
 python3 DFS.py
-echo Finished DFS.py
+echo Finished merging fasta files
 python3 pdb.py
-echo Finished pdb.py
+echo Finished alignment
