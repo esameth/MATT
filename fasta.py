f561c96d338891652207c7f68558769a055b3ded
esameth <46979903+esameth@users.noreply.github.com>
2020-03-15 14:25:06 -0400

    Increased run time

--- fasta.py
+++ fasta.py
@@ -5,7 +5,16 @@ Sequence similarity is based on Levenshtein Edit Distance between strings
 '''
 
 import json
+import Levenshtein
 from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import IUPAC
+
+class Protein:
+    def __init__(self, level, u_id):
+        self.level = level
+        self.u_id = u_id
 
 # Open the json file
 def openFile():
@@ -15,60 +24,62 @@ def openFile():
 
 # Will determine what values need to be removed
 def remove():
-    # Will hold the protein sequences as the key and its header line as the value
-    tempdict = {}
     # Used to remove sequences
     removed = {}
+    # Key = level, Value = [Sequences]
+    temp = {}
+    # Holds SeqRecords to write to a fasta file
+    records = []
 
-    with open("data/ecod.latest.fasta.txt", "rt") as f:
-        for reads in SeqIO.parse(f, 'fasta'):
+    with open("data/ecod.latest.F40.fasta.copy.fasta", "rt") as input:
+        for reads in SeqIO.parse(input, 'fasta'):
             sequence = str(reads.seq)
             ID = str(reads.id)
 
-            # Add sequence as a key to the dictionary and the ID as a value
-            if sequence not in tempdict:
-                # Used as boolean for if they are different enough to add to the dictionary
-                OK = True
+            # Get the protein hierarchy level
+            level = ID.split("|")[2]
 
-                # Get the protein hierarchy level
-                level = "|" + ID.split("|")[2] + "|"
+            # Used as boolean for if they are different enough to add to the dictionary
+            OK = True
+
+            # Increases run time - we only get the Levenshtein ratio of those of the same hierarchy
+            if level not in temp:
+                temp[level] = []
+
+            if sequence not in temp[level] and sequence not in removed:
                 # Check the keys in the dictionary of the same level to see the number of differences
-                for seq, id in tempdict.items():
+                for seq in temp[level]:
                     # If they are similar, then we will not add it to dictionary (90% sequence identity)
-                    if level in id and identity(sequence, seq) >= 0.9:
+                    if Levenshtein.ratio(sequence, seq) >= 0.9:
                         OK = False
                         break
-
                 # Enough differences so add it to the dictionary
                 if OK == True:
-                    tempdict[reads.seq] = reads.id
+                    temp[level].append(sequence)
+                    records.append(SeqRecord(Seq(sequence, IUPAC.protein), id=ID, name=ID, description=ID))
 
-            # Remove the entry if the sequence is in the dictionary already or not enough differences
-            elif sequence in tempdict or OK == False:
-                # Not enough differences so we will remove it from the file
+            if sequence in temp or OK == False:
                 if sequence not in removed:
                     removed[sequence] = []
-                removed[sequence].append((((ID.split('|')[0])[1:]), (ID.split('|')[2])))
-                
-    return tempdict, removed
+                removed[sequence].append(Protein(level, ID.split("|")[0]).__dict__)
 
-# Rewrites the fasta file so that there are no duplicates or similar sequences (< 3 differences)
-def removeFasta(tempdict):
-    # Write the dictionary values to a fasta file
-    with open("data/ecod.latest.fasta.txt", 'w') as f:
-         SeqIO.write(tempdict.values(), f, 'fasta')
+    # Rewrites the fasta file so that there are no duplicates or similar sequences (< 3 differences)
+    with open("fastacopy_test.fasta", 'w') as output:
+        SeqIO.write(records, output, 'fasta')
+
+    return removed
+
 
 # Rewrites the json file so that there are no duplicates or similar sequences (< 3 differences)
 def removeDict(proteins, removed):
     for protein in removed.values():
         for value in protein:
             # Split the id so we can parse through the proteins dictionary
-            x, h, t = value[1].split(".")[0], value[1].split(".")[1], value[1].split(".")[2]
-            f = value[1].split(".")[3] if len(value[1].split(".")) == 4 else "None"
+            x, h, t = value["level"].split(".")[0], value["level"].split(".")[1], value["level"].split(".")[2]
+            f = value["level"].split(".")[3] if len(value["level"].split(".")) == 4 else "None"
             hierarchy = proteins[x][h][t][f]
-
             # Remove the duplicate proteins in the json file
-            id = value[0]
+            id = value["u_id"]
             for i in range(len(hierarchy)):
                 if hierarchy[i]["u_id"] == id:
                     del hierarchy[i]
@@ -78,28 +89,10 @@ def removeDict(proteins, removed):
     with open("proteins.json", "w") as f:
         json.dump(proteins, f, ensure_ascii=False, indent=4)
 
-# Calculates the number of differences between two strings (the distance)
-def identity(s1,s2):
-    if len(s1) > len(s2):
-        s1,s2 = s2,s1
-    distances = range(len(s1) + 1)
-    for index2, char2 in enumerate(s2):
-        newDistances = [index2+1]
-        for index1, char1 in enumerate(s1):
-            if char1 == char2:
-                newDistances.append(distances[index1])
-            else:
-                newDistances.append(1 + min((distances[index1],
-                                             distances[index1+1],
-                                             newDistances[-1])))
-        distances = newDistances
-    return round((len(s2) - distances[-1])/len(s2), 1)
-
 def main():
     proteins = openFile()
-    tempdict, removed = remove()
-    removeFasta(tempdict)
+    removed = remove()
     removeDict(proteins, removed)
-   
+
 if __name__ == "__main__":
     main()
