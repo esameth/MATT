52881017498f971578ab43e437d921323c6e8890
esameth <46979903+esameth@users.noreply.github.com>
2020-05-02 16:47:19 -0400

    Update matt.sh

--- matt.sh
+++ matt.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-python3 parser.py
-echo Finished creating dictionary
-python3 fasta.py
-echo Finished removing duplicates
-python3 directories.py
-echo Finished creating directories
-python3 DFS.py
-echo Finished merging fasta files
-python3 pdb.py
-echo Finished alignment
+if [ $# -ne 1 ]; then
+    echo "Usage: [ECOD fasta]"
+    exit
+else
+    if [ ! -f "$1" ]; then
+        echo "'$1' does not exist"
+        echo "Usage: [ECOD fasta]"
+        exit
+    fi
+fi
+
+time python3 parser.py "$1"
+echo "Removed fasta duplicates and created directories"
+time python3 pdb.py
+echo "Finished all alignments"
