4ea4fc6c087ad18eb95eb31e1f1b3928ff0be513
esameth <46979903+esameth@users.noreply.github.com>
2020-05-12 23:02:39 -0400

    Uses defaultdict

--- parser.py
+++ parser.py
@@ -4,11 +4,16 @@ Duplicates are based on protein sequence to reduce redundancy during alignment
 Sequence similarity is based on Levenshtein Edit Distance between strings
 '''
 
+import re
 import Levenshtein
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
+from collections import defaultdict
+
+# Nested dictionary to hold our levels and the proteins in it
+proteins = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
 
 # Gets Levenshtein distance of the sequence and all values in the same level
 # Returns False if >= 90% sequence similarity
@@ -18,15 +23,27 @@ def calcDist(seq, dict):
             return False
     return True
 
-# Removes duplicates in the fasta file
-def remove(fasta):
+# Removes duplicates in the fasta file, and creates the dictionaries necessary for us to create directories
+# and to determine which proteins will be aligned together
+def parse(fasta):
     # Holds records that will be added to fasta file
     records = []
     levelDict = {}
     removed = set()
 
     for reads in SeqIO.parse(fasta, "fasta"):
-        seq, level = str(reads.seq), str(reads.id).split("|")[2]
+        id = str(reads.id).split("|")
+        seq, pdb, level, chain = str(reads.seq), id[1][1:5], id[2], re.split(':|,', id[3])
+
+        # Split chain if more than one and get only chain names
+        chain = ','.join([chain[i] for i in range(len(chain)) if i % 2 == 0])
+
+        # Get the levels
+        f_id = level.split(".")
+        x, h, t = f_id[0], f_id[1], f_id[2]
+        # Some f_id do not have a F group
+        f = f_id[3] if len(f_id) == 4 else "None"
+
         if level not in levelDict:
             levelDict[level] = []
 
@@ -38,9 +55,12 @@ def remove(fasta):
             # Enough similarities so add it to our dict and fasta file
             else:
                 levelDict[level].append(seq)
-                records.append(SeqRecord(Seq(seq, IUPAC.protein), id=reads.id))
+                records.append(SeqRecord(Seq(seq, IUPAC.protein), id=reads.id, name=reads.id, description=reads.id))
+                # Add each protein's information to the dict
+                proteins[x][h][t][f].append((pdb, chain))
+
     # Rewrites the fasta file so that there are no duplicates or similar sequences (< 3 differences)
     with open(fasta, 'w') as output:
         SeqIO.write(records, output, 'fasta')
 
-    return records
+    return levelDict.keys(), proteins
