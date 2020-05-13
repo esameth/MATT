e86a41d31dc07f785d56817841b42d7caf447a01
esameth <46979903+esameth@users.noreply.github.com>
2020-05-12 23:03:49 -0400

    Cleaned up iterating through nested dict

--- pdb.py
+++ pdb.py
@@ -1,52 +1,41 @@
 '''
-Goes through the json file and parses the pdb id to get which folder the pdb file would lie in.
+Parses the proteins dictionary to get which folder the pdb file would lie in.
 Creates a temp file of the pdb file paths that will be used as input to MATT to create alignments for each level
 The temp file is deleted when done
 '''
 
-import json
 import os
 import tempfile
 import subprocess
+
+import directories
+proteins = directories.proteins
 
 # Path to pdb files
 pdbPath = "/data/pdb"
 home = os.getcwd()
 
-# Open the json file
-def openFile():
-    with open("proteins.json") as f:
-        loaded_json = json.load(f)
-    return loaded_json
-
 # Parse through the dictionary and pull the pdb id
 # Each _pdbList will hold the pdb ids that will be in the alignment
 # Each _path will have the path so we can change to that directory for outputting the alignment
-def parse(proteins):
+def parse():
     for x, x_dict in proteins.items():
         XpdbList = []
-        Xpath = os.path.join(x, "alignments")
         for h, h_dict in x_dict.items():
             HpdbList = []
-            Hpath = os.path.join(x, h, "alignments")
             for t, t_dict in h_dict.items():
                 TpdbList = []
-                Tpath = os.path.join(x, h, t, "alignments")
-                for f, f_dict in t_dict.items():
-                    FpdbList = []
-                    Fpath = os.path.join(x, h, t, f, "alignments")
-                    for protein in f_dict:
-                        # Add the pdb ids to each list
-                        FpdbList.append((protein["pdb"], protein["chain"]))
-                        TpdbList.append((protein["pdb"], protein["chain"]))
-                        HpdbList.append((protein["pdb"], protein["chain"]))
-                        XpdbList.append((protein["pdb"], protein["chain"]))
+                for f, FpdbList in t_dict.items():
+                    # Add the pdb ids to each list
+                    TpdbList.extend(FpdbList)
+                    HpdbList.extend(FpdbList)
+                    XpdbList.extend(FpdbList)
 
                     # Create the temp file of pdb paths for aligning
-                    makeTempFile(FpdbList, Fpath)
-                makeTempFile(TpdbList, Tpath)
-            makeTempFile(HpdbList, Hpath)
-        makeTempFile(XpdbList, Xpath)
+                    makeTempFile(FpdbList, os.path.join(x, h, t, f, "alignments"))
+                makeTempFile(TpdbList, os.path.join(x, h, t, "alignments"))
+            makeTempFile(HpdbList, os.path.join(x, h, "alignments"))
+        makeTempFile(XpdbList, os.path.join(x, "alignments"))
 
 # Create a temporary file that will be passed into MATT
 def makeTempFile(pdbList, pathway):
@@ -55,7 +44,6 @@ def makeTempFile(pdbList, pathway):
     # Change to the directory that will hold the alignments
     pathway = os.path.join(home, pathway)
     os.chdir(pathway)
-
     # If alignment folder is empty, run the alignment
     if len(os.listdir(pathway)) == 0:
         # Create a tem file that will hold the pdb paths
@@ -82,5 +70,4 @@ def makeTempFile(pdbList, pathway):
     os.chdir(home)
 
 if __name__ == "__main__":
-    proteins = openFile()
-    parse(proteins)
+    parse()
